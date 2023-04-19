6014f1b9c51e2f38d6a49e8741b1f6ccffa263ae
gr404_ahrru <shootingdice@mail..ru>
2023-04-19 12:13:03 +0700

    phpmyadmin

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,18 +2,29 @@
 
 namespace Controller;
 
+use Model\Post;
 use Src\View;
+use Model\User;
+
 
 class Site
 {
     public function index(): string
     {
-        $view = new View();
-        return $view->render('site.hello', ['message' => 'index working']);
+        $posts = Post::all();
+        return (new View())->render('site.post', ['posts' => $posts]);
     }
 
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
+    }
+
 }
